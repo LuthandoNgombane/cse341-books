102c973079b84624013faf6420c799672810290f
LuthandoNgombane <luthandongombane@gmail.com>
2026-09-23 19:29:39 +0200

    Daily Dev

--- src/router.js
+++ src/router.js
@@ -6,5 +6,45 @@ const router = express.Router();
 router.get('/books', getBooksHandler);
 router.get('/books/:id', getBookByIdHandler);
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: Books returned successfully
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/books', getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     summary: Get one book by id
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The custom book id
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book returned successfully
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/books/:id', getBookByIdHandler);
+
+
 
 export default router;
